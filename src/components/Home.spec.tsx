9049365405f74e13c3da0663cd064443fc29a2fd
Yun Seob Lee <fhzotxldj@gmail.com>
2024-09-18 17:24:52 +0900

    fix type error in test file

--- src/components/Home.spec.tsx
+++ src/components/Home.spec.tsx
@@ -1,18 +1,26 @@
 import Home from "./Home";
-import { describe, expect, test, beforeAll } from "vitest";
+import { describe, expect, test, beforeAll, afterEach } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { mockIPC } from "@tauri-apps/api/mocks";
+import { mockIPC, clearMocks } from "@tauri-apps/api/mocks";
+import { InvokeArgs } from "@tauri-apps/api/core";
 
 beforeAll(() => {});
 
+afterEach(() => {
+  clearMocks();
+});
+
 describe("Home", () => {
   test("should render the Home component", () => {
-    mockIPC((cmd, args) => {
-      // simulated rust command called "add" that just adds two numbers
+    const mockCommandHandler = <T,>(cmd: string, _: InvokeArgs | undefined): Promise<T> => {
       if (cmd === "get_chroma_version") {
-        return "0.1.0";
+        return Promise.resolve("0.1.0" as unknown as T); // casting string to T
+      } else {
+        return Promise.resolve("unknown command" as unknown as T); // casting string to T
       }
-    });
+    };
+
+    mockIPC(mockCommandHandler);
 
     render(<Home />);
 
