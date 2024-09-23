d2b972ca392a3d73257d8097600ac5a2466fdcae
Yun Seob Lee <fhzotxldj@gmail.com>
2024-09-23 21:22:07 +0900

    add resizable layout and table's auto truncate

--- src/components/Collections.tsx
+++ src/components/Collections.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   // Table,
@@ -22,6 +22,11 @@ import {
   getCoreRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+import { useResizable } from "react-resizable-layout";
+import { cn } from "../utils/cn";
+import "../styles/collection.css";
+import { embeddingToString } from "../utils/embeddingToString";
+import { MiddleTruncate } from "@re-dev/react-truncate";
 
 const Collections: React.FC = () => {
   const currentCollection = useSelector<State, string>(
@@ -29,6 +34,16 @@ const Collections: React.FC = () => {
   );
   const [embeddings, setEmbeddings] = React.useState<EmbeddingsData[]>([]);
   const [loading, setLoading] = React.useState(true);
+  const {
+    isDragging: isTerminalDragging,
+    position: terminalH,
+    splitterProps: terminalDragBarProps,
+  } = useResizable({
+    axis: "y",
+    initial: 10,
+    min: 30,
+    reverse: true,
+  });
 
   const columnHelper = createColumnHelper<EmbeddingsData>();
 
@@ -37,8 +52,18 @@ const Collections: React.FC = () => {
       cell: (info) => info.getValue(),
       footer: (info) => info.column.id,
     }),
+    columnHelper.accessor("document", {
+      cell: (info) => (
+        <MiddleTruncate end={0}>{info.getValue()}</MiddleTruncate>
+      ),
+      footer: (info) => info.column.id,
+    }),
     columnHelper.accessor("embedding", {
-      cell: (info) => info.getValue(),
+      cell: (info) => (
+        <MiddleTruncate end={0}>
+          {embeddingToString(info.getValue())}
+        </MiddleTruncate>
+      ),
       footer: (info) => info.column.id,
     }),
   ];
@@ -74,65 +99,86 @@ const Collections: React.FC = () => {
 
   return (
     <Box minH="100vh" width={"100%"}>
-      {currentCollection}
       {loading ? (
         <Heading>Loading...</Heading>
       ) : embeddings.length === 0 ? (
         <Heading>It's empty.</Heading>
       ) : (
-        <Box>
-          <Flex>
-            <Box>collection id</Box>
-            <Box>dimensions: {embeddings[0].embedding.length}</Box>
-          </Flex>
-          <table>
-            <thead>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <tr key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => (
-                    <th key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
+        <Box
+          className={
+            "flex flex-column h-screen bg-dark font-mono color-white overflow-hidden"
+          }
+        >
+          <Box className={"flex grow"}>
+            <Box width={'100%'}>
+              <Flex>
+                <Box>collection id</Box>
+                <Box>dimensions: {embeddings[0].embedding.length}</Box>
+              </Flex>
+              <table width={'100%'}>
+                <thead>
+                  {table.getHeaderGroups().map((headerGroup) => (
+                    <tr key={headerGroup.id}>
+                      {headerGroup.headers.map((header) => (
+                        <th key={header.id}>
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(
+                                header.column.columnDef.header,
+                                header.getContext()
+                              )}
+                        </th>
+                      ))}
+                    </tr>
+                  ))}
+                </thead>
+                <tbody>
+                  {table.getRowModel().rows.map((row) => (
+                    <tr key={row.id}>
+                      {row.getVisibleCells().map((cell) => (
+                        <td key={cell.id}>
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
                           )}
-                    </th>
+                        </td>
+                      ))}
+                    </tr>
                   ))}
-                </tr>
-              ))}
-            </thead>
-            <tbody>
-              {table.getRowModel().rows.map((row) => (
-                <tr key={row.id}>
-                  {row.getVisibleCells().map((cell) => (
-                    <td key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </td>
+                </tbody>
+                <tfoot>
+                  {table.getFooterGroups().map((footerGroup) => (
+                    <tr key={footerGroup.id}>
+                      {footerGroup.headers.map((header) => (
+                        <th key={header.id}>
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(
+                                header.column.columnDef.footer,
+                                header.getContext()
+                              )}
+                        </th>
+                      ))}
+                    </tr>
                   ))}
-                </tr>
-              ))}
-            </tbody>
-            <tfoot>
-              {table.getFooterGroups().map((footerGroup) => (
-                <tr key={footerGroup.id}>
-                  {footerGroup.headers.map((header) => (
-                    <th key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.footer,
-                            header.getContext()
-                          )}
-                    </th>
-                  ))}
-                </tr>
-              ))}
-            </tfoot>
-          </table>
+                </tfoot>
+              </table>
+            </Box>
+          </Box>
+          <Splitter
+            {...terminalDragBarProps}
+            dir="horizontal"
+            isDragging={isTerminalDragging}
+          />
+          <Box
+            className={cn(
+              "shrink-0 bg-darker contents",
+              isTerminalDragging && "dragging"
+            )}
+            style={{ height: terminalH }}
+          >
+            Placeholder
+          </Box>
         </Box>
       )}
     </Box>
@@ -140,3 +186,23 @@ const Collections: React.FC = () => {
 };
 
 export default Collections;
+
+const Splitter = ({ id = "drag-bar", dir, isDragging, ...props }: any) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  return (
+    <Box
+      id={id}
+      data-testid={id}
+      tabIndex={0}
+      className={cn(
+        "sample-drag-bar",
+        dir === "horizontal" && "sample-drag-bar--horizontal",
+        (isDragging || isFocused) && "sample-drag-bar--dragging"
+      )}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+      {...props}
+    />
+  );
+};
