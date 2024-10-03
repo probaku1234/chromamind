be0f1ff847074de4899b0a83ca41bae333d75ab8
Yun Seob Lee <fhzotxldj@gmail.com>
2024-10-03 09:21:29 +0900

    edit ui of App page

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,11 @@ import {
   FormLabel,
   Heading,
   Icon,
-  Image,
   Input,
-  Link,
   Spinner,
-  Stack,
   Text,
 } from '@chakra-ui/react'
 import { invoke } from '@tauri-apps/api/core'
-import reactLogo from './assets/react.svg'
 import { CheckCircleIcon } from '@chakra-ui/icons'
 import { getCurrentWindow } from '@tauri-apps/api/window'
 import './App.css'
@@ -54,38 +50,11 @@ const App: React.FC = () => {
   return (
     <Container maxW="container.md" centerContent height={'100vh'}>
       <Heading as="h1" my={4}>
-        Welcome to Tauri!
+        Connect to your Chroma
       </Heading>
 
-      <Stack direction="row" spacing={4} my={4}>
-        <Link href="https://vitejs.dev" isExternal>
-          <Image
-            src="/chroma-seeklogo.png"
-            className="logo vite"
-            alt="Vite logo"
-            boxSize="50px"
-          />
-        </Link>
-        <Link href="https://tauri.app" isExternal>
-          <Image
-            src="/tauri.svg"
-            className="logo tauri"
-            alt="Tauri logo"
-            boxSize="50px"
-          />
-        </Link>
-        <Link href="https://reactjs.org" isExternal>
-          <Image
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-            boxSize="50px"
-          />
-        </Link>
-      </Stack>
-
       <Text my={4}>
-        Click on the Tauri, Vite, and React logos to learn more.
+        Type the url of your chroma
       </Text>
 
       <Box
